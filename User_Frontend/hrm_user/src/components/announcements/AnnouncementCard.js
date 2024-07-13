a4a6eaf70699925968b93ee6be209fd10a058afe
lakshankd <dominiclakshank@gmail.com>
2024-07-13 21:12:53 +0530

    chore: Update AnnouncementCard styling to remove unnecessary classes

--- User_Frontend/hrm_user/src/components/announcements/AnnouncementCard.js
+++ User_Frontend/hrm_user/src/components/announcements/AnnouncementCard.js
@@ -5,7 +5,7 @@ const AnnouncementCard = ({ subject, date, message }) => {
   return (
     <div
       id="alert-additional-content-1"
-      className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+      className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50"
       role="alert"
     >
       <div className="flex items-center">
@@ -21,7 +21,7 @@ const AnnouncementCard = ({ subject, date, message }) => {
       </div>
       <div className="mt-2 mb-4 text-sm">{message}</div>
       <div className="flex">
-        <div className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <div className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700">
           {date}
         </div>
       </div>
