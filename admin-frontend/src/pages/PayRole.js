b7257304a08b968949eb88089afe58b35087b1d5
lakshankd <dominiclakshank@gmail.com>
2024-07-17 23:58:20 +0530

    chore: Remove unnecessary name prop in PayRole component

--- admin-frontend/src/pages/PayRole.js
+++ admin-frontend/src/pages/PayRole.js
@@ -125,7 +125,7 @@ const PayRole = () => {
   return (
     <div className="flex flex-col bg-[#d0e0e5] min-h-[100vh] ml-[220px]">
       <div className="flex flex-col pl-10 pt-5">
-        <Welcome name="Welcome Lakmini" tab="PayRoles" />
+        <Welcome tab="PayRoles" />
         <div className="flex flex-row md:w-[96.4%] mt-[25px] justify-between gap-x-3">
           <div className="w-[350px]">
             <CommonSearchableSelect
