3aec13bd6234592b1436fc8b49e64020e5578cf2
salinda-yasas-99 <salindasym@gmail.com>
2024-07-17 22:30:14 +0530

    ading payslip

--- User_Frontend/hrm_user/src/pages/PayRole.js
+++ User_Frontend/hrm_user/src/pages/PayRole.js
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Welcome from "../components/Welcome";
+import { getAllPaySheets } from "../Services/PaySlipApi";
 
 const PayRole = () => {
-  const [payRole, setPayRole] = useState([
-    {
-      payMonth: "January",
-      date: "2024-03-05",
-      IssuedDate: "2024-06-01",
-      basicAmount: "100,000",
-    },
-    {
-      payMonth: "February",
-      date: "2024-03-05",
-      IssuedDate: "2024-06-01",
-      basicAmount: "100,000",
-    },
-    {
-      payMonth: "March",
-      date: "2024-03-05",
-      IssuedDate: "2024-06-01",
-      basicAmount: "100,000",
-    },
-    {
-      payMonth: "April",
-      date: "2024-03-05",
-      IssuedDate: "2024-06-01",
-      basicAmount: "100,000",
-    },
-  ]);
+  const [payRole, setPayRole] = useState([]);
+
+  const fetchPaySheets = async () => {
+    const fetched = await getAllPaySheets();
+    setPayRole(fetched);
+  };
+
+  useEffect(() => {
+    fetchPaySheets();
+  }, []);
   return (
     <div className="flex flex-col bg-[#d0e0e5] min-h-[100vh] ml-[220px]">
       <div className="flex flex-col pl-10 pt-5">
@@ -41,7 +26,7 @@ const PayRole = () => {
                     Pay Month
                   </th>
                   <th scope="col" class="px-6 py-5">
-                    Date
+                    Pay Date
                   </th>
                   <th scope="col" class="px-6 py-5">
                     Issued date
@@ -55,26 +40,52 @@ const PayRole = () => {
                 </tr>
               </thead>
               <tbody>
-                {payRole.map((payItem, key) => {
-                  return (
-                    <tr
-                      id={key}
-                      className="bg-white border-b text-gray-900 font-medium"
-                    >
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {payItem.payMonth}
-                      </td>
-                      <td className="px-6 py-4">{payItem.date}</td>
-                      <td className="px-6 py-4">{payItem.IssuedDate}</td>
-                      <td className="px-6 py-4">{payItem.basicAmount}</td>
-                      <td className="px-6 py-4">
-                        <div className="bg-[#497cc9] flex justify-center py-[5px] rounded-md">
-                          View
-                        </div>
-                      </td>
-                    </tr>
-                  );
-                })}
+                {payRole.length > 0 ? (
+                  payRole.map((payItem, key) => {
+                    return (
+                      <tr
+                        id={key}
+                        className="bg-white border-b text-gray-900 font-medium"
+                      >
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {payItem.month}
+                        </td>
+                        <td className="px-6 py-4">
+                          {new Date(payItem.payDate).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                            }
+                          )}
+                        </td>
+                        <td className="px-6 py-4">
+                          {new Date(payItem.issuedDate).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                            }
+                          )}
+                        </td>
+                        <td className="px-6 py-4">{payItem.basicAmount}</td>
+                        <td className="px-6 py-4">
+                          <div className="bg-[#497cc9] flex justify-center py-[5px] rounded-md">
+                            View
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr className="bg-white border-b text-gray-900 font-medium">
+                    <td colSpan="5" className="text-center px-6 py-4">
+                      No payslips available
+                    </td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
