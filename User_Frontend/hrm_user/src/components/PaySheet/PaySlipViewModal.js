bb3effa1f2144f47502a7eca0d14fbf7edd7083a
salinda-yasas-99 <salindasym@gmail.com>
2024-07-18 23:22:02 +0530

    adding paysheet download

--- User_Frontend/hrm_user/src/components/PaySheet/PaySlipViewModal.js
+++ User_Frontend/hrm_user/src/components/PaySheet/PaySlipViewModal.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 const PaySlipViewModal = ({
   handleClosePaySlipViewModal,
   paySlipViewModalData,
 }) => {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
   return (
     <div
       id="crud-modal"
       tabindex="-1"
       aria-hidden="true"
       className="fixed inset-0 flex items-center justify-center z-50 "
+      ref={componentRef}
     >
       <div className="relative p-4 w-full max-w-md max-h-full ">
         <div className="relative bg-white rounded-lg shadow border-black border-solid border-[0.09rem]">
@@ -186,6 +192,13 @@ const PaySlipViewModal = ({
               onClick={handleClosePaySlipViewModal}
             >
               Close
+            </button>
+            <button
+              type="button"
+              className="ml-2 text-white inline-flex items-center bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              onClick={handlePrint}
+            >
+              Print
             </button>
           </form>
         </div>
